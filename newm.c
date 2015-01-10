134ff8a531fa365886946a3980a08fda863ead53
Benji Wiebe <benjiwiebe14@gmail.com>
2015-01-10 14:34:02 -0600

    Removed pointless printing of output to stderr; changed to stdout

--- newm.c
+++ newm.c
@@ -48,14 +48,14 @@ int main()
 		struct userlist *ls = ul_create(8);
 		ul_populate(ls);
 		ul_sort(ls);
-		fprintf(stderr, "Users logged in: ");
+		printf("Users logged in: ");
 		for(int i = 0; ls->array[i] != NULL; i++)
 		{
 			if(i > 0 && !strcmp(ls->array[i], ls->array[i-1]))
 				continue;
-			fprintf(stderr, "%s, ", ls->array[i]);
+			printf("%s, ", ls->array[i]);
 		}
-		fprintf(stderr, "\b\b  \n");
+		printf("\b\b  \n");
 		ul_free(ls);
 	}
 
